f7ac8bc732a04c48253f6987715ae0dc1d8070a8
Viniciusmbc <viniciuscaceres@gmail.com>
2022-05-12 21:22:16 -0300

    Add prometheus integration

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Viniciusmbc/dio-expert-session-finance/adapter/http/actuator"
 	"github.com/Viniciusmbc/dio-expert-session-finance/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Init() {
@@ -12,6 +13,6 @@ func Init() {
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
 
 	http.HandleFunc("/health", actuator.Health)
-
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
